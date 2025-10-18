bcd20f5cf520d4bb2b78fa9bce7301c11130136c
Praduman rawat <pradumanrawat123@gmail.com>
2025-10-18 19:55:46 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+    JWT_SECRET: process.env.JWT_SECRET,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    TAVILY_API_KEY: process.env.TAVILY_API_KEY,
+    MONGO_URI: process.env.MONGO_URI,
+  },
+};
 
 export default nextConfig;
